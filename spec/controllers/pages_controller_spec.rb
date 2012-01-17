c29b63f7c326669b676306f3af70eba7649306dc
David Haynes <drhaynes2@gmail.com>
2012-01-17 12:30:25 +0000

    Refactored pages_controller_spec.rb to be tidier.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+  end
+
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -12,8 +16,7 @@ describe PagesController do
     it "should have the correct title" do
       get 'home'
       response.should have_selector("title", 
-                         :content => 
-                          "Ruby on Rails Tutorial Sample App | Home")
+                         :content => @base_title + "Home")
     end
   end
 
@@ -25,8 +28,7 @@ describe PagesController do
      it "should have the correct title" do
       get 'contact'
       response.should have_selector("title", 
-                         :content => 
-                          "Ruby on Rails Tutorial Sample App | Contact")
+                         :content => @base_title + "Contact") 
     end
   end
   
@@ -38,8 +40,7 @@ describe PagesController do
     it "should have the correct title" do
       get 'about'
       response.should have_selector("title", 
-                         :content => 
-                          "Ruby on Rails Tutorial Sample App | About")
+                         :content => @base_title + "About")
     end
   end
 
@@ -51,8 +52,7 @@ describe PagesController do
     it "should have the correct title" do
       get 'help'
       response.should have_selector("title",
-                        :content =>
-                         "Ruby on Rails Tutorial Sample App | Help")
+                        :content => @base_title + "Help")
     end
   end
 
