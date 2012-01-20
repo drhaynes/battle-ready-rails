96489194f69630b5c6405e4acda26032d62ecfac
David Haynes <drhaynes2@gmail.com>
2012-01-20 15:33:04 +0000

    Started to add users. Added signup page.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -20,4 +20,9 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+
+  it "should have a Signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
 end
