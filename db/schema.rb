804033f40e22426e67a99677210fa187cd8eb06e
David <drh@akira-ubuntu.(none)>
2012-02-02 22:56:52 +0000

    Updated spec/spec_helper.rb to fix some odd block syntax errors. Also updated for 3.2 config, and Spork.

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20120126151048) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.string   "encrypted_password"
     t.string   "salt"
   end
