6abf7fd1cf013d3e79bd43d1615352037d120dd6
drhaynes <drhaynes2@gmail.com>
2012-02-03 02:56:48 +0000

    Modified user model tests to work with new has_secure_password implementation. All tests now pass.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -21,42 +21,5 @@ class User < ActiveRecord::Base
                     :format     => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
 
-  # Automatically create virtual password_confirmation attr
-  validates :password, :presence     => true,
-                       :confirmation => true,
-                       :length       => { :within => 6..40 }
-
-  before_save :encrypt_password
-
-  # Return true if the user's password matches the submitted password
-  def has_password?(submitted_password)
-    encrypted_password == encrypt(submitted_password)
-  end
-
-  def self.authenticate(email, submitted_password)
-    user = find_by_email(email)
-    return nil if user.nil?
-    return user if user.has_password?(submitted_password)
-  end
-
-  private
-
-    def encrypt_password
-      # We don't want to change the salt if it exists already
-      self.salt = make_salt if new_record?
-      self.encrypted_password = encrypt(password)
-    end
-
-    def encrypt(string)
-      secure_hash("#{salt}--#{string}")
-    end
-
-    def make_salt
-      # Use current utc time to create salt
-      secure_hash("#{Time.now.utc}--#{password}")
-    end
-
-    def secure_hash(string)
-      Digest::SHA2.hexdigest(string)
-    end
+  validates :password, presence: true
 end
