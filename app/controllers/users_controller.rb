a5bd5866019859babe9e95b046a60aed96f3bf42
drhaynes <drhaynes2@gmail.com>
2012-02-03 04:48:24 +0000

    Signup page now redirects to new user page on successful submission. All tests pass.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # handle successful save
+      redirect_to @user
     else
       render 'new'
     end
