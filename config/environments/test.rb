506ef36f2958981e3dda944e1afa5ae6b44ac3c1
drhaynes <drhaynes2@gmail.com>
2012-02-03 03:26:45 +0000

    Lowered bcrypt cost setting to minimum to speed up tests.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,10 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by decreasing BCrypt cost function
+  require "bcrypt"
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
