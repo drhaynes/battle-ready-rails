b1cc304e85e66244c0ce976e60c0ef5b73d87c8f
drhaynes <drhaynes2@gmail.com>
2012-02-07 19:40:42 +0000

    Added settings and users links to navbar.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,6 +31,8 @@ describe "AuthenticationPages" do
       before { valid_signin(user) }
 
       it { should have_selector('title', text: user.name) }
+      it { should have_link('Users', href: users_path) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
