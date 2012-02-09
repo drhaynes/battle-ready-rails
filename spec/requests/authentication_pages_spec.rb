914021d6a4c22a3922e73b09c2477c2f4032383b
drhaynes <drhaynes2@gmail.com>
2012-02-09 13:06:18 +0000

    Users now required to sign in to edit profile.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -43,4 +43,24 @@ describe "AuthenticationPages" do
       end
     end
   end
+
+  describe "authorisation" do
+
+    describe "for non-signed-in users" do
+      let(:user) { Factory(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end
+  end
 end
